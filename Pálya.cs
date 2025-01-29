12c8154ffe7d7349acd1bd8b93e3774e57213577
Szemethy <szemethy.levente@students.jedlik.eu>
2025-01-29 11:18:41 +0100

    első gondolathullámok

---
+++ Pálya.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienBlast
+{
+    class Pálya
+    {
+
+        public void LoadMap(string FilePath)
+        {
+            string[] sorok = File.ReadAllLines("Pálya.txt", Encoding.UTF8);
+
+            int sorHossz = sorok.Length;
+            int sorMagasság = sorok.Length;
+        }
+
+        
+    }
+}
