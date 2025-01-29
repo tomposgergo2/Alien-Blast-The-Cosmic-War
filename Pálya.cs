b535cb0b16b9d5517e3b933fdec572ecde9af4ca
Szemethy <szemethy.levente@students.jedlik.eu>
2025-01-29 11:31:44 +0100

    ma utolsó commit

--- Pálya.cs
+++ Pálya.cs
@@ -10,12 +10,29 @@ namespace AlienBlast
     class Pálya
     {
 
-        public void LoadMap(string FilePath)
+        public void LoadMap()
         {
             string[] sorok = File.ReadAllLines("Pálya.txt", Encoding.UTF8);
 
+
+            int index = 0;
             int sorHossz = sorok.Length;
-            int sorMagasság = sorok.Length;
+            int sorMagasság = sorok.Length; //ez így még nem jó
+
+            int[,] map = new int[sorHossz, sorMagasság];
+
+            foreach (var sor in sorok)
+            {
+                if(sorok.)
+            }
+
+
+
+
+
+
+            return map;
+
         }
 
         
