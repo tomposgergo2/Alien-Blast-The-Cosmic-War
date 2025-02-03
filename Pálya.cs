245ef224baf88ddbd029ec5f863b11967bc9da7d
Szemethy <szemethy.levente@students.jedlik.eu>
2025-02-03 10:02:49 +0100

    újragondolás

--- Pálya.cs
+++ Pálya.cs
@@ -4,23 +4,55 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace AlienBlast
 {
     internal class Pálya
     {
         public List<string[]> Pályák { get; private set; }
+        public Canvas canvas { get; private set; }
 
-        public Pálya()
-        {
+        public Pálya(Canvas canvas)
+        {         
             Pályák = new List<string[]>();
             Beolvasás();
+            this.canvas = canvas;
+        }
+
+        public void Generálás(int pályaindex)
+        {
+           
+
+            foreach (var pályaSor in Pályák)
+            {
+                for (int y = 0; y < pályaSor.Length; y++)
+                {
+                    for (int x = 0; x < pályaSor[y].Length; x++)
+                    {
+
+                        if (pályaSor[y][x] == '1')
+                        {
+                            var négyzet = new System.Windows.Shapes.Rectangle
+                            {
+                                Width = 96,
+                                Height = 96,
+                                Fill = System.Windows.Media.Brushes.Orange,
+                            };
+                            // na majd it a Zalánnal lesz pár mondatom
+
+                            Canvas.SetLeft(négyzet, x * 96); // ez az x
+                            Canvas.SetTop(négyzet, y * 96);  // ez az y 
+                            canvas.Children.Add(négyzet);
+                        }
+                    }
+                }
+            }
         }
 
         private void Beolvasás()
         {
             string[] sorok = File.ReadAllLines("Pálya.txt");
-
 
 
             List<string> pálya = new List<string>();
